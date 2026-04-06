2c8fd925cd4aa4c6c3e60db998a74b9025bef4e5
poky1084 <nanohtcop@gmail.com>
2026-04-06 16:46:06 +0200

    set default values for increase percent

--- Bomber_GUI/Properties/Settings.Designer.cs
+++ Bomber_GUI/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Bomber_GUI.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -73,7 +73,7 @@ namespace Bomber_GUI.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("100")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
         public decimal PercentOnLoss {
             get {
                 return ((decimal)(this["PercentOnLoss"]));
@@ -361,7 +361,7 @@ namespace Bomber_GUI.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("100")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
         public decimal precentOnWin {
             get {
                 return ((decimal)(this["precentOnWin"]));
