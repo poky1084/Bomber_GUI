914ba332bd65240454eb0bd40a6cc77eb83c7c0e
poky1084 <nanohtcop@gmail.com>
2026-04-08 19:46:30 +0200

    Bomber BOT #2 - get cookie

--- Bomber_GUI/Forms/SettingsForm.cs
+++ Bomber_GUI/Forms/SettingsForm.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿
+using Keno;
+using Newtonsoft.Json;
 using RestSharp;
 using System;
 using System.Collections.Generic;
@@ -10,6 +12,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.Menu;
 
 namespace Bomber_GUI.Forms
@@ -235,8 +238,8 @@ namespace Bomber_GUI.Forms
             //return;
             LoadingDefaults = true;
 
-            //textBox1.Text = Properties.Settings.Default.Cookie;
-            //textBox2.Text = Properties.Settings.Default.Agent;
+            textBox1.Text = Properties.Settings.Default.Cookie;
+            textBox2.Text = Properties.Settings.Default.Agent;
 
             checkInstant.Checked = Properties.Settings.Default.Instant;
             pHash.Text = Properties.Settings.Default.PlayerHash;
@@ -335,8 +338,8 @@ namespace Bomber_GUI.Forms
 
         public void initSettings()
         {
-            
-                    
+
+
             GameConfig.ConfigTag = coinList[coinIndex];
             int BetAmmount = (int)numberofBets.Value;
             if (GameConfig.UseStrat)
@@ -345,8 +348,8 @@ namespace Bomber_GUI.Forms
             GameConfig.Proxy = proxyBox.Text;
 
             GameConfig.Instant = checkInstant.Checked;
-            //GameConfig.Agent = textBox2.Text;
-            //GameConfig.Cookie = textBox1.Text;
+            GameConfig.Agent = textBox2.Text;
+            GameConfig.Cookie = textBox1.Text;
             GameConfig.PlayerHash = pHash.Text;
             GameConfig.BetAmmount = BetAmmount;
             GameConfig.BetCost = betCostNUD.Value;
@@ -358,8 +361,8 @@ namespace Bomber_GUI.Forms
             GameConfig.BombCount = (int)BombCountBox.Value;
             GameConfig.ShowGameBombs = showGBombsCheck.Checked;
             GameConfig.SaveLogToFile = saveLog.Checked;
-    
-            
+
+
             GameConfig.SiteConfig = SiteConfig.Text;
             GameConfig.StopAfterGamesAmmount = (int)stopAfterGamesNum.Value;
             GameConfig.StopAfterGames = stopAfterGamesChecked.Checked;
@@ -409,8 +412,8 @@ namespace Bomber_GUI.Forms
             //Properties.Settings.Default.UseProxy = GameConfig.UseProxy;
             //GameConfig.Proxy = proxyBox.Text;
             Properties.Settings.Default.Instant = checkInstant.Checked;
-           // Properties.Settings.Default.Agent = textBox2.Text;
-           // Properties.Settings.Default.Cookie = textBox1.Text;
+            Properties.Settings.Default.Agent = textBox2.Text;
+            Properties.Settings.Default.Cookie = textBox1.Text;
             Properties.Settings.Default.PlayerHash = pHash.Text;
             Properties.Settings.Default.BetAmmount = BetAmmount;
             Properties.Settings.Default.BetCost = betCostNUD.Value;
@@ -472,41 +475,32 @@ namespace Bomber_GUI.Forms
         {
             PutBalance(false);
         }
-        private async Task<string> GraphQL(string operationName, string query,
-                                  BetClass variables = null)
-        {
-            var url = "https://" + SiteConfig.Text + "/_api/graphql";
 
-            var body = new BetSend
-            {
-                operationName = operationName,
-                query = query,
-                variables = variables
-            };
-
-            var options = new
-            {
-                method = "POST",
-                headers = new Dictionary<string, string>
-        {
-            { "Content-Type", "application/json" },
-            { "x-access-token", pHash.Text }
-        },
-                body = body
-            };
-
-            return await BrowserFetch.FetchAsync(url, options);
-        }
         public async void PutBalance(bool sign)
         {
             try
             {
-                var json = await GraphQL(
-                    "UserBalances",
-                    "query UserBalances {\n  user {\n    id\n    balances {\n      available {\n        amount\n        currency\n        __typename\n      }\n      vault {\n        amount\n        currency\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n"
-                );
+                var mainurl = "https://" + SiteConfig.Text + "/_api/graphql";
+                var request = new RestRequest(Method.POST);
+                var client = new RestClient(mainurl);
+                client.CookieContainer = cc;
+                client.UserAgent = textBox2.Text; ;
+                client.CookieContainer.Add(new Cookie("cf_clearance", textBox1.Text, "/", SiteConfig.Text));
+                BetQuery payload = new BetQuery();
+                payload.operationName = "UserBalances";
+                payload.query = "query UserBalances {\n  user {\n    id\n    balances {\n      available {\n        amount\n        currency\n        __typename\n      }\n      vault {\n        amount\n        currency\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n";
+                //request.RequestFormat = DataFormat.Json;
+                request.AddHeader("Content-Type", "application/json");
+                request.AddHeader("x-access-token", pHash.Text);
 
-                BalancesData response = JsonConvert.DeserializeObject<BalancesData>(json);
+                request.AddParameter("application/json", JsonConvert.SerializeObject(payload), ParameterType.RequestBody);
+                //request.AddJsonBody(payload);
+                //IRestResponse response = client.Execute(request);
+
+                var restResponse =
+                    await client.ExecuteAsync(request);
+
+                BalancesData response = JsonConvert.DeserializeObject<BalancesData>(restResponse.Content);
 
                 if (response.errors != null)
                 {
@@ -536,11 +530,12 @@ namespace Bomber_GUI.Forms
                                 if (balance.amount > 0)
                                 {
                                     cfgTag.Items.Add($"{balance.currency} - {balance.amount.ToString("N8")}");
-                                }
+                                } 
                                 else
                                 {
                                     cfgTag.Items.Add(response.data.user.balances[i].available.currency);
                                 }
+                                
                             }
 
                             if (response.data.user.balances[i].available.currency == currentSelected.ToLower())
@@ -655,6 +650,56 @@ namespace Bomber_GUI.Forms
         private void nudDelay_ValueChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnWebViewLogin_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void SiteConfig_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SiteConfig_TextChanged_1(object sender, EventArgs e)
+        {
+            GameConfig.SiteConfig = SiteConfig.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (var loginForm = new WebViewLogin(SiteConfig.Text))
+            {
+                // ShowDialog blocks until user clicks Done (DialogResult.OK)
+                var result = loginForm.ShowDialog(this);
+
+                if (result == DialogResult.OK)
+                {
+                    // Apply captured values
+                    GameConfig.Cookie = loginForm.CapturedClearance;
+                    GameConfig.Agent = loginForm.CapturedUserAgent;
+
+                    // Update UI text fields
+                    textBox1.Text = GameConfig.Cookie;
+                    textBox2.Text = GameConfig.Agent;
+
+                    // Save to settings
+                    Properties.Settings.Default.Cookie = GameConfig.Cookie;
+                    Properties.Settings.Default.Agent = GameConfig.Agent;
+                    Properties.Settings.Default.Save();
+
+                    // Rebuild CookieContainer fresh
+                    cc = new CookieContainer();
+
+                    // Verify connection with new cookies
+                    //await Authorize();
+                }
+                else
+                {
+                    
+                }
+            }
         }
     }
 }
